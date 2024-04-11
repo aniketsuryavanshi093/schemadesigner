1848ab019f555b4c9ef8caf2a6780045138975f8
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2024-04-11 09:53:00 +0530

    quick fixes

--- src/app/dashboard/folder/[id]/page.tsx
+++ src/app/dashboard/folder/[id]/page.tsx
@@ -1,16 +1,22 @@
-'use server'
-import { useParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import { useParams } from "next/navigation";
+import React from "react";
+import Commonheader from "../../DashboardComponent/Commonheader";
 
-const Folder = async () => {
-    const { id } = useParams()
+const Folder = () => {
+  const { id } = useParams();
+  const HeaderComponnet = () => {
     return (
-        <Commonheader
-        title="My favorites
-"
-      />
-        <div>Folder</div>
-    )
-}
+      <p className="mb-0 text-2xl font-semibold leading-6 text-gray-700">
+        FolderName
+      </p>
+    );
+  };
+  return (
+    <div>
+      <Commonheader HeaderComponent={HeaderComponnet} />
+    </div>
+  );
+};
 
-export default Folder
+export default Folder;
