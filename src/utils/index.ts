4d0c97a97176d8d1e162923b8ca664e61777ecc7
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2024-07-04 18:13:12 +0530

    solved document undefined isue

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -112,30 +112,37 @@ const generateURL = (payload: Record<string, any>): string => {
 export default generateURL;
 
 export function setCookie(name: string, value: string, daysToExpire: number) {
-  const date = new Date();
-  date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
-  const expires = "expires=" + date.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  if (typeof window !== "undefined") {
+    const date = new Date();
+    date.setTime(date.getTime() + (daysToExpire * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  }
 }
 
 // Function to get a cookie
 export function getCookie(name: string) {
-  const cookieName = name + "=";
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(';');
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(cookieName) === 0) {
-      return cookie.substring(cookieName.length, cookie.length);
+  if (typeof window !== "undefined") {
+    {
+      const cookieName = name + "=";
+      const decodedCookie = decodeURIComponent(document.cookie);
+      const cookieArray = decodedCookie.split(';');
+      for (let i = 0; i < cookieArray.length; i++) {
+        let cookie = cookieArray[i];
+        while (cookie.charAt(0) === ' ') {
+          cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(cookieName) === 0) {
+          return cookie.substring(cookieName.length, cookie.length);
+        }
+      }
+      return "";
     }
   }
-  return "";
 }
-
 // Function to delete a cookie
 export function deleteCookie(name: any) {
-  document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  if (typeof window !== "undefined") {
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+  }
 }
